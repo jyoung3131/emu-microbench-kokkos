f26bfb2f61998f3f38ccbc9d146ac0c1c16c8382
Jeff Young <jyoung9@gatech.edu>
2019-09-19 20:57:23 -0400

    Updated libkokkos with mdspanII fixes. Updated sim tests

--- run_sim_tests.sh
+++ run_sim_tests.sh
@@ -1,6 +1,6 @@
 #Simple script to run different microbench tests
 MODE=serial_spawn
-LOG2ELEM=10
-THREADS=128
+LOG2ELEM=4
+THREADS=32
 ITERATIONS=1
-emusim.x local_stream_kokkos_1909.mwx $MODE $LOG2ELEM $THREADS $ITERATIONS   
+emusim.x --untimed_short_trace --gcs_per_nodelet 4 --initialize_mem -m30 --max_sim_time 400 local_stream_kokkos.mwx $MODE $LOG2ELEM $THREADS $ITERATIONS   
