2229a6820f432da5e2569465ae7ffcd7d978b85b
Jeff Young <jyoung9@gatech.edu>
2019-10-24 21:26:39 -0400

    Fixing permissions errors and renaming kokkos hello world

--- run_sim_tests.sh
+++ run_sim_tests.sh
@@ -1,6 +1,8 @@
 #Simple script to run different microbench tests
 MODE=serial_spawn
 LOG2ELEM=4
-THREADS=32
+THREADS=64
 ITERATIONS=1
-emusim.x --untimed_short_trace --gcs_per_nodelet 4 --initialize_mem -m30 --max_sim_time 400 local_stream_kokkos.mwx $MODE $LOG2ELEM $THREADS $ITERATIONS   
+emusim.x --ignore_starttiming --max_sim_time 400 local_stream_kokkos.mwx $MODE $LOG2ELEM $THREADS $ITERATIONS   
+#emusim.x --untimed_short_trace --gcs_per_nodelet 4 --initialize_mem -m30 --max_sim_time 400 local_stream_kokkos.mwx $MODE $LOG2ELEM $THREADS $ITERATIONS   
+#emusim.x --max_sim_time 400 local_stream_kokkos.mwx $MODE $LOG2ELEM $THREADS $ITERATIONS   
