5747b7b89d2d24b49be04ebdb65973ca07ac27d8
Jeff Young <jyoung9@gatech.edu>
2020-06-18 15:24:23 -0400

    Updated tarball scripts

--- tests/create_emu_hw_tarball.sh
+++ tests/create_emu_hw_tarball.sh
@@ -4,6 +4,8 @@
 # 6/13/20
 # Jeff Young
 
+EMU_TEST_DIR=emu_hw_test
+
 if [ -z $1 ];
 then
 	printf "Pass a test directory as the first arg\n"
@@ -11,5 +13,10 @@ then
 	exit 1
 fi
 
-#Create a tarball with the test scripts and mwx directories
-tar cvzf $1.tgz generate_tests.py *.sh *.json test_suites/ ../build-kokkos-hw/*.mwx
+#Create a tarball with the test scripts and mwx directories under a new top-level directory
+mkdir -p ${EMU_TEST_DIR}
+cp -rf $1 runall_tests_serial.sh print_results_awk.sh set_emu_hw_paths.sh ../bin/*.mwx ${EMU_TEST_DIR}/.
+#Tar up the test directory
+tar cvzf $1.tgz ${EMU_TEST_DIR}
+#Remove the local copy of this directory
+rm -rf ${EMU_TEST_DIR}
